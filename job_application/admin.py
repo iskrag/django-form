86ad58ba7beac563402298ad615a32090978c013
iskra <iskrag@gmail.com>
2024-06-07 17:00:51 +0200

    Add admin interface customisation features

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Form
 
-# to register database form with the admin interface
-admin.site.register(Form)
+
+class FormAdmin(admin.ModelAdmin):
+    # variable names must not be changed
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('date', 'occupation')
+    ordering = ('first_name', )
+    readonly_fields = ('occupation', )
+
+
+# to register database and admin forms with the admin interface
+admin.site.register(Form, FormAdmin)
 
