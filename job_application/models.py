4062a8b6c924e668a581103c767cf9a3ae24f672
iskra <iskrag@gmail.com>
2024-06-07 17:17:31 +0200

    Create a base HTML template

--- job_application/models.py
+++ job_application/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Form(models.Model):
+    """Form, which defines the database model"""
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
     email = models.EmailField()
--- job_application/urls.py
+++ job_application/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.index, name='index')]
+# variable name must not be changed
+urlpatterns = [path('', views.index, name='index'),
+               path('about/', views.about, name='about')]
